e724700565867564a9d6711b92b377baa8560579
luasuarez <luissuarez2098@gmail.com>
2022-09-22 15:14:23 -0500

    commit test 3

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -5,42 +5,54 @@
 // No cambies los nombres de las funciones.
 
 function imprimirSumaNumeros() {
-    // Imprime la suma de los números del 1 al 10
-    // Pista: usa un acumulador
-    // Tu código:
+  // Imprime la suma de los números del 1 al 10
+  // Pista: usa un acumulador
+  // Tu código:
+  var acumulador = 0;
+  for (var i = 1; i <= 10; i++) {
+    acumulador += i;
+  }
+  return acumulador;
 }
 
-function encuentraPares(array){
+function encuentraPares(array) {
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  return array.filter((num) => num % 2 === 0);
 }
 
-function elevaAlCuadrado(array){
+function elevaAlCuadrado(array) {
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  var cuadrado = [];
+  for (var i = 0; i < array.length; i++) {
+    cuadrado.push(Math.pow(array[i], 2));
+  }
+  return cuadrado;
 }
 
-function sumaArray(array){
+function sumaArray(array) {
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  return array.reduce((a, b) => a + b, 0);
 }
 
-function numeroDigitos(num){
+function numeroDigitos(num) {
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  return num.toString().length;
 }
-  
-  
-  // No modificar nada debajo de esta línea
-  // --------------------------------
-  
-  module.exports = {
-    imprimirSumaNumeros,
-    encuentraPares,
-    elevaAlCuadrado,
-    sumaArray,
-    numeroDigitos
-  };
+
+// No modificar nada debajo de esta línea
+// --------------------------------
+
+module.exports = {
+  imprimirSumaNumeros,
+  encuentraPares,
+  elevaAlCuadrado,
+  sumaArray,
+  numeroDigitos,
+};
